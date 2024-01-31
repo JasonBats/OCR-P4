4f3a963b22f8b5fac0de42c79e97bd4e8761be3e
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-01-31 10:58:09 +0100

    Docstring added. Fixed score inputs stacking.

--- model/match.py
+++ model/match.py
@@ -11,25 +11,27 @@ class Match:
         self.match_data = [(player_1, score_1), (player_2, score_2)]
 
     def encounter(self):
+        """
+        User inputs the first player's score.
+        The second player's score is determined by the first one.
+        This function also verifies if the user is sure about the outcome he wrote.
+        The function just updates the scores to the tournament's data.
+        """
         tournament_view = view.view.TournamentView()
-        self.score_1 = tournament_view.write_score(self.player_1)
-        if self.score_1 == "1":
-            self.score_2 = "0"
-        elif self.score_1 == "0":
-            self.score_2 = "1"
-        elif self.score_1 == "0.5":
-            self.score_2 = "0.5"
-        else:
-            view.view.TournamentView.incorrect_score()
-            self.encounter()
-        verification = tournament_view.score_review(self.player_1, self.player_2, self.score_1, self.score_2)
-        if verification == 1:
-            pass
-        elif verification == 2:
-            self.encounter()
-        else:
-            view.view.TournamentView.incorrect_verif()
-            self.encounter()
+        while True:
+            self.score_1 = tournament_view.write_score(self.player_1)
+            if self.score_1 in ["1", "0.5", "0"]:
+                self.score_2 = "0" if self.score_1 == "1" else "1" if self.score_1 == "0" else "0.5"
+
+                verification = tournament_view.score_review(self.player_1, self.player_2, self.score_1, self.score_2)
+                if verification == 1:
+                    break
+                elif verification == 2:
+                    continue
+                else:
+                    view.view.TournamentView.incorrect_verif()
+            else:
+                view.view.TournamentView.incorrect_score()
 
     def __repr__(self):
         return f"\n{self.player_1} {self.score_1} VS {self.score_2} {self.player_2}\n"
